6cdbe2bd009cd7f4a9324672a769f3e0d0c793fc
Brian Hsu <brianhsu.hsu@gmail.com>
2014-12-15 12:42:20 +0800

    Add SyncTwoSite scripts.

---
+++ SyncTwoSite/dailyDump/dump.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#dateString=`date --date='yesterday'`
+#dateString="2014-10-09"
+dateString=$1
+
+######## OUTPUT SETTING ##################
+exportDir="export$dateString"
+exportFile="$dateString.tar.gz"
+
+######## START ####################
+
+cd "$(dirname "$0")"
+echo "Dump mongoDB collection $dateString..."
+mongodump --db zhenhaiDaily -o $exportDir --collection $dateString
+mongodump --db zhenhaiDaily -o $exportDir --collection cached
+
+tar -cvzf $exportFile $exportDir/zhenhaiDaily/
+#rm -rvf $exportDir
